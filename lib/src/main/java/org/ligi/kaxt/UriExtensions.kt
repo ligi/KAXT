39c6d40335d477e284512cd50f0b62ac93f4f1cf
ligi <ligi@ligi.de>
2016-11-15 16:11:01 +0100

    Fix import

--- lib/src/main/java/org/ligi/kaxt/UriExtensions.kt
+++ lib/src/main/java/org/ligi/kaxt/UriExtensions.kt
@@ -3,6 +3,7 @@ package org.ligi.kaxt
 
 import android.content.Context
 import android.net.Uri
+import org.ligi.kaxt.converter.ImageFromIntentUriToFileConverter
 import java.io.File
 
 fun Uri.loadImage(context: Context): File? {
