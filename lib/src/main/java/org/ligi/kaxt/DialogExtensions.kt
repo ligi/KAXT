e7439d9c4020d9d473d2b519e4887f3b86319c51
ligi <ligi@ligi.de>
2016-11-27 11:41:14 +0100

    Add extension to set a button on alertdialog with a resourceId for the label

--- lib/src/main/java/org/ligi/kaxt/DialogExtensions.kt
+++ lib/src/main/java/org/ligi/kaxt/DialogExtensions.kt
@@ -1,6 +1,9 @@
 package org.ligi.kaxt
 
+import android.app.AlertDialog
 import android.app.Dialog
+import android.content.DialogInterface
+import android.support.annotation.StringRes
 
 fun Dialog.dismissIfShowing() {
     if (isShowing) {
@@ -13,4 +16,10 @@ fun Dialog?.dismissIfNotNullAndShowing() {
     if (this != null) {
         dismissIfShowing()
     }
+}
+
+fun AlertDialog.setButton(whichButton: Int, @StringRes label: Int, action: () -> Unit) {
+    setButton(whichButton,context.getString(label),{ dialogInterface: DialogInterface, i: Int ->
+        action.invoke()
+    })
 }
