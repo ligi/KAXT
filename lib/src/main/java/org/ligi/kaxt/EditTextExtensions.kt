627c129a3368f2896bece5773edc3b9ea19d57df
ligi <ligi@ligi.de>
2017-01-11 13:25:59 +0100

    Improve code

--- lib/src/main/java/org/ligi/kaxt/EditTextExtensions.kt
+++ lib/src/main/java/org/ligi/kaxt/EditTextExtensions.kt
@@ -4,15 +4,10 @@ import android.text.Editable
 import android.text.TextWatcher
 import android.widget.EditText
 
-fun EditText.doAfterEdit(foo: (editable: Editable) -> Unit) = this.addTextChangedListener(object : TextWatcher {
-    override fun afterTextChanged(editable: Editable) {
-        foo(editable)
-    }
+fun EditText.doAfterEdit(function: (editable: Editable) -> Unit) = addTextChangedListener(object : TextWatcher {
 
-    override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-    }
-
-    override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-    }
+    override fun afterTextChanged(editable: Editable) = function(editable)
+    override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) = Unit
+    override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) = Unit
 
 })
